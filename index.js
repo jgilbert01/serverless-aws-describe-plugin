a676fbf6e9fc968d625581e3338fe1ad178ecb84
John Gilbert <john.gilbert@devtechnology.com>
2022-06-16 16:00:26 -0400

    seed

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ class Plugin {
       awsdesc: {
         async resolve({ address }) {
           const [service, describe, key, id, l1, l2] = address.split('.');
-          // console.log('service: ', service);
-          // console.log('describe: ', describe);
-          // console.log('key: ', key);
-          // console.log('id: ', id);
-          // console.log('l1: ', l1);
-          // console.log('l2: ', l2);
-          // const data = await serverless.getProvider('aws').request('DynamoDB', 'describeTable', {
+          // console.log({
+          //   service, describe, key, id, l1, l2,
+          // });
+
           const data = await serverless.getProvider('aws').request(service, describe, {
             [key]: id,
           });
